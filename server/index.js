b4c84f595579ab3d2994668c8bb17c2afa91bd25
rado <rado_g@abv.bg>
2019-12-19 10:25:21 +0200

    server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
+require('./util/service-locator').initialize(config);
+
+
+require('./config/database')(config);
+const storage = require('./config/storage')(config);
+const app = require('express')();
+const cors = require ('cors');
+app.use(cors());
+require('./config/express')(app, storage);
+require('./config/routes')(app);
+require('./config/passport')(app);
+
+
+app.listen(config.port);
+console.log('Listening on port ' + config.port);
